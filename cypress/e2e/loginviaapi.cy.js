9749ed2915ef8d308ac15dc5cd1440ea4177fa77
Rajan Chaudhary <rajann44@gmail.com>
2023-02-14 20:42:18 +0100

    simple login via post request

---
+++ cypress/e2e/loginviaapi.cy.js
@@ -0,0 +1,21 @@
+import action from "../utils/Action"
+
+describe('login via api', () => {
+
+    it('Simple API login', () => {
+        cy.request('POST', 'https://ums.ninox.com/api/credentials/signin', {
+            email: 'gibeyi7348@iucake.com',
+            password: 'Gibeyi7348@iucake.com',
+        })
+        action.navigateTo('https://app.ninox.com/')
+        action.sleepForSeconds(2)
+        action.navigateTo('https://admin.ninox.com/profile')
+        action.sleepForSeconds(2)
+      })
+
+    it('Complex API login', () => {
+        // TODO: Implement this
+        action.navigateTo('app.timelyapp.com/')
+    })
+
+})
